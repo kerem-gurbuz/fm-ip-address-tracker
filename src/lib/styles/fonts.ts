ab30cf19cf0821ddb2feac3327715ac16ae86164
Kerem Gurbuz <grbz.kerem@gmail.com>
2024-09-28 21:01:27 +0300

    build: init shadcn-ui component library

---
+++ src/lib/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { Rubik } from "next/font/google";
+
+export const rubik = Rubik({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  style: "normal",
+  display: "swap",
+  variable: "--font-rubik",
+});
